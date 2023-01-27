6d47766b9ef03c4e9b5908e62af8a6c4bd8df7a4
ryan <ryanjcua@msn.com>
2023-01-27 14:44:38 -0800

    bump elixir version and add tool_versions for asdf install

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Recipebook.MixProject do
     [
       app: :recipebook,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.14.1",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix | Mix.compilers()],
       start_permanent: Mix.env() == :prod,
